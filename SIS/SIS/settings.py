506b15604efcf5051d5cbe8d096c3191c1016772
CaptainSparky20 <39871359+CaptainSparky20@users.noreply.github.com>
2025-08-22 20:04:19 +0800

    Update MEDIA_ROOT configuration for user uploads based on environment

--- SIS/SIS/settings.py
+++ SIS/SIS/settings.py
@@ -135,7 +135,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
@@ -151,5 +150,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Media files (user uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Use Render persistent disk if available, otherwise fall back to local media folder
+if os.environ.get("RENDER"):
+    MEDIA_ROOT = '/var/media'   # must match your Render disk mount path
+else:
+    MEDIA_ROOT = BASE_DIR / 'media'
